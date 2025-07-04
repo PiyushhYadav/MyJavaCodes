d5ed0ceb3a5b8253b0ab4b54317bdc1713230637
Peaceeful <ypiyush6111@gmail.com>
2025-07-04 23:24:02 +0530

    Few basics

---
+++ src/Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        System.out.println("Hey Who are you?");
+        Scanner input = new Scanner (System.in);
+        System.out.println(input.nextInt());
+    }
+}
+//for Integers
---
+++ src/Primitives.java
@@ -0,0 +1,10 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'P';
+        float marks = 12.23f;
+        double largeDecimalNumbers = 873278328323232.3323;
+        long largeIntegerValues = 22222222244444444l;
+
+    }
+}
